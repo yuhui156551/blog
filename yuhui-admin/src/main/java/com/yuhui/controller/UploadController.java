4cbfb1cc132ff1b9750d778135e7afec25687afa
yuhui <536430462@qq.com>
2023-01-10 10:23:00 +0800

    上传图片--写博文

---
+++ yuhui-admin/src/main/java/com/yuhui/controller/UploadController.java
@@ -0,0 +1,25 @@
+package com.yuhui.controller;
+
+import com.yuhui.domain.ResponseResult;
+import com.yuhui.service.UploadService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+/**
+ * @author yuhui
+ * @date 2023/1/10 10:08
+ */
+@RestController
+public class UploadController {
+    @Autowired
+    private UploadService uploadService;
+
+    @PostMapping("/upload")
+    public ResponseResult upload(@RequestParam("img") MultipartFile multipartFile){
+        return uploadService.uploadImg(multipartFile);
+    }
+}
